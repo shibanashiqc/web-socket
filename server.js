5541fe731a8ca6b6dd76b947d145b48631d7fa41
Shiban Ashiq <shibanashiq743@gmail.com>
2023-06-04 00:08:38 +0530

    sdsdsd

--- server.js
+++ server.js
@@ -1,38 +1,20 @@
-// Import path and url dependencies
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-// Get the directory and file path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import WebSocket from 'ws';
+// start the server and specify the port number
+const port = 3000;
+const wss = new WebSocket.Server({ port: port }); 
+console.log(`[WebSocket] Starting WebSocket server on localhost:${port}`);
 
-// Import express, expressWs, and http
-import express from 'express'
-import expressWs from 'express-ws'
-import http from 'http'
-let port = 3000;
-
-
-let app = express();
-let server = http.createServer(app).listen(port);   
-console.log("Server is running on port " + port); 
-
-// Apply expressWs
-expressWs(app, server);
-
-app.use(express.static(__dirname + '/views'));
-
-// Get the route / 
-app.get('/', (req, res) => {
-    res.status(200).send("Welcome to our app");
-});
-
-// This lets the server pick up the '/ws' WebSocket route
-app.ws('/ws', async function(ws, req) {
-    // After which we wait for a message and respond to it
-    ws.on('message', async function(msg) {
-        // If a message occurs, we'll console log it on the server
-        console.log(msg);
-        // Start listening for messages
-    });
-});
+// Broadcast aka send messages to all connected clients
+wss.on('connection', (ws, request) => {
+    const clientIp = request.connection.remoteAddress;
+    console.log(`[WebSocket] Client with IP ${clientIp} has connected`); 
+    ws.send('Thanks for connecting to this nodejs websocket server');
+// Broadcast aka send messages to all connected clients
+ ws.on('message', (message) => { 
+    wss.clients.forEach((client) => {
+         if (client.readyState === WebSocket.OPEN) {
+             client.send(message); } });
+              console.log(`[WebSocket] Message ${message} was received`); 
+            });
+     });
